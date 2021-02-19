3a9d9f8a6a943d70b6e53b9bea30ee3558a3a496
Frank Warstat <github.202102@smilingpirate.de>
2021-02-19 17:31:49 +0100

    Final solution for Kata with debug prints still in the code

--- word_mesh.py
+++ word_mesh.py
@@ -1,16 +1,65 @@
-
 def word_mesh(words):
-    wordBefore=""
+    resultString = ""
+    previousWord=""
     for currentWord in words:
-        print(currentWord)
-        if wordBefore =="":
-            wordBefore=currentWord
+        print("Current Word: '" + currentWord + "'")
+        print("=======================================")
+        foundMesh = False
+        if previousWord == "":
+            #first run
+            previousWord = currentWord
+            #treat first round as true
+            foundMesh = True
         else:
-            for character in currentWord:
-                    wordBefore.find(character)
-    return ""
+            #compare longest chain in current word with last chars from previous
+            for i in range(min(len(currentWord), len(previousWord)), 1,-1):
+                print("Checking: '" + currentWord[:i] + "' against: '" + previousWord[-i:] + "'")
+                if currentWord[:i] == previousWord[-i:]:
+                    resultString += currentWord[:i]
+                    foundMesh = True
+                    break
+        
+        if foundMesh == False:
+            return "failed to mesh"
 
-print(word_mesh(["beacon", "condominium", "umbilical", "california"]), "conumcal")
+        previousWord = currentWord
+
+    print("Result: "+resultString)
+
+    return resultString
+
 print(word_mesh(["allow", "lowering", "ringmaster", "terror"]), "lowringter")
 print(word_mesh(["abandon", "donation", "onion", "ongoing"]), "dononon")
 print(word_mesh(["jamestown", "ownership", "hippocampus", "pushcart", "cartographer", "pheromone"]), "ownhippuscartpher")
+print(word_mesh(["beacon", "condominium", "umbilical", "california"]), "conumcal")
+
+#this idea was too complicated overall:
+# def word_mesh2(words):
+    resultString = ""
+    previousWord=""
+    for currentWord in words:
+        print(currentWord)
+        if previousWord =="":
+            previousWord = currentWord
+        else:
+            subStringFind = 0
+            #check if first chars of second word is in first word
+            for i in range(1, len(currentWord)):
+                subStringFind = previousWord.rfind(currentWord[:i])
+                print("Checking: '" + currentWord[:i] + "'")
+                print("Found at: " + str(subStringFind))
+                #check, whether we actually found at the END of the word before
+                if subStringFind == -1:
+                    print("Substring '" + currentWord[:i] + "' Not found at all")
+                    break
+                elif i + subStringFind == len(previousWord):
+                    print("OK, this is the end")
+                    #concat the result
+                    resultString += currentWord[:i]
+                    break
+                else:
+                    print("damn, somewhere else")
+        previousWord = currentWord
+        print("Result: "+resultString)
+
+    return resultString
