60db3056378edf9a606e9e1020af90182c1cbb7c
Frank Warstat <github.202102@smilingpirate.de>
2021-02-23 22:33:37 +0100

    new bart and co from Juergen

---
+++ bartundco.py
@@ -0,0 +1,33 @@
+namelist = [{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}]
+names = namelist[0:]
+print(names)
+names1 = []
+
+def namelistFunction(names):
+
+        for i, j in enumerate(namelist):
+            blabla = 5
+            names1.append(namelist[i].values())
+        return names1
+
+print(namelistFunction(names))
+
+print(blabla)
+
+#names2 = []
+#print ("+".join(namelist))
+# for val in namelist.itervalues():
+#   print(val)
+# namelist.values("name")
+# for item in namelist:
+#   item.values(namelist)
+#  list.append(item)
+# print(list)
+# dict.keys()
+# namelist = {'name': 'Bart', 'name': 'Lisa', 'name': 'Maggie'}
+# new_list = list(namelist.values())
+# print (new_list)
+# def namelist):
+# !!!!print("Namen:", namelist)
+# result = str(namelist).replace("name", "").split(":")
+# print(new_list)
