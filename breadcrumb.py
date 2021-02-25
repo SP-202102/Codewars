fe1ec133d942ba467997b6f6b87312c39f4a3b76
Frank Warstat <github.202102@smilingpirate.de>
2021-02-25 16:16:11 +0100

    some syntax fixes , rewriting breadcrump

--- breadcrumb.py
+++ breadcrumb.py
@@ -1,16 +1,44 @@
-def generate_bc(inputUrl):
+class urlHandler():
+    elementList = []
+    def splitUrl(url, separator ="/" ):
+        elementList = url.split( separator )
+        print("Element List:", elementList)
+
+
+    def getBcMenu( url, separator=" : " ):
+        splitUrl( url)
+        return (buildOutPutUrl(separator))
+        
+        
+    def buildOutPutUrl(separator):
+        return( separator.join(elementList))
+
+
+
+def generate_bc(url, separator):
     outputHtml = ""
-# split input
-    elementList = inputUrl.split( "/" )
+    outputHtml = getBcMenu( url, separator )
 
-# apply rules
+    print(outputHtml) 
 
-#formate output elements
+
+# split input / check for python html functions
+    # for htmlElement in parser:
+    #     print( htmlElement )
+
+    
+
+# make first part "HOME"
+
+# all other parts except last:
+    # strip last part?
+
+    # apply rules for short
+
+
+    #formate output elements incl make them links
 
 # construct output
-    outputHtml = "/".join(elementList)
-    print(elementList)
-    print(outputHtml)
-    return (outputHtml)
-    
-print(generate_bc( "test/test2"))
+
+
+print(generate_bc( "mysite.com/pictures/holidays.html", " : "))
