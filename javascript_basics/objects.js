0f745fcb2f0e91b1622dc05ee258e6530a162bae
Timm <timmwarstat@googlemail.com>
2021-03-03 00:06:07 +0100

    js and ts erste schritte

---
+++ javascript_basics/objects.js
@@ -0,0 +1,12 @@
+let person = {
+    name: 'tim', age: 28
+};
+
+
+//dot notation
+person.name = 'Tim Warstat'
+
+//bracket notation
+let selection = 'name'
+person[selection] = 'Tim pow Warstat'
+console.log(person);
